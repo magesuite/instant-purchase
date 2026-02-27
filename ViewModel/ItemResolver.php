9d396daabfd4b869f4aef284218ee9a49e2388d8
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2026-02-27 08:51:23 +0100

    fix: [IPET-355] Fix case when rendered product doesn't exist in Magento

--- ViewModel/ItemResolver.php
+++ ViewModel/ItemResolver.php
@@ -7,13 +7,19 @@ namespace MageSuite\InstantPurchase\ViewModel;
 class ItemResolver implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     public function __construct(
-        protected \Magento\Catalog\Model\Product\Configuration\Item\ItemResolverInterface $itemResolver,
+        protected \Magento\Catalog\Model\Product\Configuration\Item\ItemResolverInterface $itemResolver
     ) {
     }
 
-    public function getFinalProduct(\Magento\Catalog\Model\Product\Configuration\Item\ItemInterface $item): \Magento\Catalog\Api\Data\ProductInterface
+    public function getFinalProduct(\Magento\Catalog\Model\Product\Configuration\Item\ItemInterface $item): ?\Magento\Catalog\Api\Data\ProductInterface
     {
-        return $this->itemResolver->getFinalProduct($this->resolveItem($item));
+        $item = $this->resolveItem($item);
+
+        if (!$item->getProduct() || !$item->getProduct()->getId()) {
+            return null;
+        }
+
+        return $this->itemResolver->getFinalProduct($item);
     }
 
     public function resolveItem(\Magento\Catalog\Model\Product\Configuration\Item\ItemInterface $item): \Magento\Catalog\Model\Product\Configuration\Item\ItemInterface
